9b250cb4c5aa0dfecfce74d23d9bda9afcb74980
johnnash2025 <aws.cloudpattern@gmail.com>
2025-02-23 15:43:18 +0900

    added AppRouter function

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  experimental: {
+    // @ts-ignore: experimental.appDirが型定義にないため無視します
+    appDir: true,
+  },
 };
 
 export default nextConfig;
